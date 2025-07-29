cb1197146c2cd5fde5e0a4f5b57e03f808c6be10
bell <bellelm@Mac.lan>
2025-07-29 21:24:17 +0100

    densified second model with scaling and pca and made a original kodel with sparse data to compare both

--- src/main.py
+++ src/main.py
@@ -36,28 +36,39 @@ X_train, X_test, y_train, y_test = train_test_split(
        random_state=42,
        stratify=y
 )
+
 #Impute nan/missing values
 imputer = SimpleImputer(strategy='mean')
 X_train_imputed = imputer.fit_transform(X_train)
 X_test_imputed = imputer.transform(X_test)
+
+#Original model withoit scaling and PCA
+model=RandomForestClassifier(random_state=42)
+model.fit(X_train_imputed, y_train)
+
+#Evaluate original model
+y_pred = model.predict(X_test_imputed)
+print("First model eval: ",classification_report(y_test, y_pred))
 
 #Scale trainig and test data
 scaler = StandardScaler(with_mean=False)  # with_mean=False for sparse matrices
 X_train_scaled = scaler.fit_transform(X_train_imputed) #fit and transform only on training data
 X_test_scaled= scaler.transform(X_test_imputed)
 
+#convert sparse matrices to dense
+X_train_scaled_dense = X_train_scaled.toarray()  
+X_test_scaled_dense = X_test_scaled.toarray()
+
 #Apply PCA to capture 95% variance
-pca=PCA(n_components=100)
-X_train_pca = pca.fit_transform(X_train_scaled)
-X_test_pca = pca.transform(X_test_scaled)
+pca=PCA(n_components=0.95)
+X_train_pca = pca.fit_transform(X_train_scaled_dense)
+X_test_pca = pca.transform(X_test_scaled_dense)
 
-#Classifier model
-model=RandomForestClassifier(random_state=42)
-model.fit(X_train_pca, y_train)
+#2nd Classifier model with scaled and PCA transformed data
+model2=RandomForestClassifier(random_state=42)
+model2.fit(X_train_pca, y_train)
 
-#Evaluate model
-y_pred = model.predict(X_test_pca)
-print(classification_report(y_test, y_pred))
-
-
+#Evaluate model2
+y_pred2 = model2.predict(X_test_pca)
+print("Second model eval: ",classification_report(y_test, y_pred2))
 

c04e952ce7f35686875655a125d9a4495ccfc9d9
bell <bellelm@Mac.lan>
2025-08-05 20:03:22 +0100

    err handling

--- backend/tests/routers/test_predictions.py
+++ backend/tests/routers/test_predictions.py
@@ -6,7 +6,7 @@ client=TestClient(app)
 
 class TestPredictionsRouter:
     
-    def test_predict_test_origin_success(self):
+    def test_predict_text_origin_success(self):
         """Test successful prediction via HTTP POST"""
 
         payload = {
@@ -22,3 +22,29 @@ class TestPredictionsRouter:
         assert data["result"] in ["human_written", "ai_generated", "uncertain"]
         assert data["confidence"] > 0
         assert data["text_length"] == len(payload["text"])
+    
+    def test_text_too_short(self):
+        """Test to see error display if text is too short"""
+        payload={
+            "text":"no",
+            "content_type": "essay"
+        }
+        response = client.post(f"{settings.API_V1_STR}/predictions/", json=payload)
+        assert response.status_code==400
+        assert response.json()["detail"]== "Text must be at least 10 characters long"
+    
+    def test_if_value_is_valid(self):
+        """Test to see error display if content types in post request is valid"""
+        payload={
+            "text":"yes i was unable to move! it hurt!",
+            "content_type": "random"
+        }
+        response = client.post(f"{settings.API_V1_STR}/predictions/", json=payload)
+        assert response.status_code==422
+    def test_if_missing_keys(self):
+        """Test to see error display for when missing keys"""
+        payload={
+            "content_type": "essay"
+        }
+        response = client.post(f"{settings.API_V1_STR}/predictions/", json=payload)
+        assert response.status_code==422

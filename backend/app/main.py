4c7d69e736e4207d035f9fedb28b1c935fd68236
bell <bellelm@Mac.lan>
2025-08-22 17:31:39 +0100

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,26 @@ from fastapi import FastAPI
 from app.config import settings
 from app.routers import health
 from app.routers import predictions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version="1.0.0",
     debug=settings.DEBUG
+)
+origins = [
+    "http://localhost:3000", 
+    "http://localhost:5173",  
+    "https://textguard.netlify.app",  
+    "https://textguard.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
 )
 
 app.include_router(health.router, prefix=settings.API_V1_STR, tags=["health"])

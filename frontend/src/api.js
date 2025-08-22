36726fafd079cbf780a4fdfd724f5af380185186
bell <bellelm@Mac.lan>
2025-08-22 17:21:00 +0100

    added axios and state

---
+++ frontend/src/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const textGuardAPI = axios.create({
+    baseURL: 'https://textguardapi-production.up.railway.app/api/v1'
+})
+
+export const postPredict=(text,contentType)=>{
+    return textGuardAPI.post("/predictions/",{text, content_type:contentType})
+    .then(({data})=>{
+        return data
+    })
+}
--- frontend/src/components/DashBoard.jsx
+++ frontend/src/components/DashBoard.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react"
+import { postPredict } from "../api";
+
 export default function DashBoard() {
+    const [text, setText] = useState("")
+    const [contentType, setContentType] = useState("")
+    const [result, setResult] = useState(null)
+
+    const [loading, setLoading] = useState(false)
+    const [err, setErr] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        postPredict(text, contentType)
+            .then((prediction) => {
+                setResult(prediction)
+            })
+            .catch((e) => {
+                setErr(true)
+                console.log(e)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+    if (loading) return <div className="loader-container"><h1 className="loader"></h1></div>
+    if (err) return <p className="error">something went wrong</p>
     return (
         <div>
             <h1 className="Header">
@@ -10,22 +37,35 @@ export default function DashBoard() {
                         Paste your text content here:
                     </h3>
                     <div>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <textarea
                                 className="text-input"
                                 placeholder="Paste your text here..."
                                 rows="10"
+                                onChange={(e) => setText(e.target.value)}
                             />
-                            <select className="content-dropdown">
+                            <select
+                                className="content-dropdown"
+                                value={contentType}
+                                onChange={(e) => setContentType(e.target.value)}>
                                 <option value="">Select content type...</option>
-                                <option value="article">Article</option>
+                                <option value="academic_paper">Acedemic paper</option>
                                 <option value="essay">Essay</option>
-                                <option value="email">Email</option>
-                                <option value="story">Story</option>
-                                <option value="review">Review</option>
-                                <option value="social">Social Media Post</option>
-                                <option value="other">Other</option>
+                                <option value="article">Article</option>
+                                <option value="creative_writing">Creative writing</option>
+                                <option value="news_article">News article</option>
+                                <option value="social_media">Social Media</option>
+                                <option value="product_review">Product review</option>
+                                <option value="blog_post">Blog post</option>
                             </select>
+                            <button
+                                className='submit-button'
+                                type="submit"
+                                disabled={!contentType || !text.trim() || text.length < 10}
+                            >Get Results</button>
+                            {text.trim().length > 0 && text.trim().length < 10 && (
+                                <p className="error-text">Please enter at least 10 characters</p>
+                            )}
                         </form>
                     </div>
                 </div>

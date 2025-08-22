17e411498d66e937b14b5b1251bb255140a20519
bell <bellelm@Mac.lan>
2025-08-22 18:24:03 +0100

    result componenet with results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import DashBoard from './components/DashBoard'
+import { Results } from './components/Results'
 
 function App() {
-return (
-<DashBoard />
-)
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<DashBoard />}></Route>
+        <Route path='/results' element={<Results />}></Route>
+      </Routes>
+    </>
+
+  )
 }
 
 export default App
--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,3 +1,26 @@
-export function Results(){
-    
+import { useLocation, useNavigate } from 'react-router-dom';
+export function Results() {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const prediction = location.state?.prediction
+
+    if (!prediction) {
+        navigate('/')
+        return null
+    }
+
+    return (
+        <div className='result-container'>
+            {/* <img className="logo" src='/textguard.png' /> */}
+            <h1 className='results'>Results</h1>
+            <div className='results-col'>
+            <p>Prediction: {prediction.result}</p>
+            <p>Confidence: {prediction.confidence}%</p>
+            <p>Processing Time: {prediction.processing_time_ms}</p>
+            <p>{prediction.timestamp}</p>
+            </div>
+            
+            <button className='back-button' onClick={() => navigate('/')}>Predict New Text</button>
+        </div>
+    )
 }
